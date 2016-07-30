bd900144825575010bc6d615c93ddb39b83d4222
David Valachovic <AdenFlorian@gmail.com>
2016-07-30 18:10:18 -0500

    fixed security issues and ignoring .bat fiels from xpi

--- data/my-script.js
+++ data/my-script.js
@@ -1,4 +1,8 @@
 var repoUrl = $(".clone-options > .input-group > .js-url-field").attr("value");
 var repoName = $("meta[property='og:title']").attr("content");
+var classes = "btn btn-outline get-repo-btn tooltipped tooltipped-s tooltipped-multiline btn-block";
+var ariaLabel = "Clone " + repoName + " to your computer and open it in SourceTree.";
+var innerText = "Open in SourceTree";
+var href = "sourcetree://cloneRepo/" + repoUrl;
 
-$(".get-repo-modal > .mt-2").append('<a href="sourcetree://cloneRepo/'+repoUrl+'" class="btn btn-outline get-repo-btn tooltipped tooltipped-s tooltipped-multiline btn-block" aria-label="Clone '+repoName+' to your computer and open it in SourceTree.">Open in SourceTree</a>');
+$(".get-repo-modal > .mt-2").append($("<a>", { href: href, class: classes, 'aria-label': ariaLabel }).text(innerText));
